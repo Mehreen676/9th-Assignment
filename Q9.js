61ab6eaa9051dc70e88622066fb86f410f043531
Mehreen676 <159542833+Mehreen676@users.noreply.github.com>
2024-02-23 21:25:46 +0500

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,4 @@
+"use strict";
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let numb = 7;
+console.log(`${numb} is my favorite number,its also my lucky number.`);
---
+++ Q9.ts
@@ -0,0 +1,3 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let numb:number =7
+console.log(`${numb} is my favorite number,its also my lucky number.`)
